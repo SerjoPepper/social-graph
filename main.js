a885aac90429d715991a2317ce6acb83da2214f7
SerjoPepper <SerjoPepper@gmail.com>
2011-09-24 21:41:14 +0400

    try to list friends

--- main.js
+++ main.js
@@ -2,9 +2,24 @@
 $(function () {
 VK.init(function(data) {
 
-console.log(data);
-console.log(VK)
-
+VK.api(
+    'friends.get',
+    { fields: 'uid,first_name,last_name,sex,photo' },
+    function (data) {
+        if (data.error)
+            return;
+        
+        var friends = data.response;
+        for (var i = 0, il = friends.length; i < il; i++) {
+            var friend = friends[i],
+                p = document.createElement('p');
+            
+            p.innerHTML = '
+                <img src="' + friend.photo + '" />
+            ' + [friend.uid, friend.first_name, friend.last_name, friend.sex].join(' ') + '<br />';
+            document.body.appendChild(p);
+        }
+    });
 });
 });
 })(jQuery);
